210b475480e9012c77bdf1388913cd60eb3d8acf
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-10-31 21:31:51 +0100

    Worked on desktop homepage

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-let moreTextButton = document.querySelectorAll('.more-text__button')
-let moreText = document.querySelectorAll('.more-text')
-let collapseTextButton = document.querySelectorAll('.collapse-text__button')
+//mobile nav bar
+let mobileNavToggle = document.querySelector('.mobile-nav__toggle');
+let mobileNav = document.querySelector('.mobile-nav');
+let backdrop = document.querySelector('.backdrop');
+
+//content-card actions
+let moreTextButton = document.querySelectorAll('.more-text__button');
+let moreText = document.querySelectorAll('.more-text');
+let collapseTextButton = document.querySelectorAll('.collapse-text__button');
+
+mobileNavToggle.addEventListener('click', function () {
+    mobileNav.style.display='block';
+    backdrop.style.display='block';
+})
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display='none';
+    backdrop.style.display='none';
+})
 
 for (let i=0; i < moreTextButton.length; i++) {
     moreTextButton[i].addEventListener('click', function() {
         moreText[i].style.display='block';
         moreTextButton[i].style.display='none';
-}
-)
+})
 }
 
 for (let i=0; i < collapseTextButton.length; i++) {
     collapseTextButton[i].addEventListener('click', function() {
         moreText[i].style.display='none';
         moreTextButton[i].style.display='block';
-    }
-    )
+})
 }
